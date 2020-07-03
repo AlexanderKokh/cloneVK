5e0e29d6dfd4b7b5823c397f12558687025100eb
IOS <alekokh@yandex.ru>
2020-07-04 02:26:42 +0400

    UI lesson 1

--- cloneVK/ViewController.swift
+++ cloneVK/ViewController.swift
@@ -1,4 +1,3 @@
-//
 //  ViewController.swift
 //  cloneVK
 //
@@ -10,11 +9,61 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var scrollViewLogin: UIScrollView!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShown(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(notification:)),name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(hideKeyBoard))
+        scrollViewLogin.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc func keyboardWillShown(notification: Notification) {
+        let info  = notification.userInfo! as NSDictionary
+        let kbSize = (info.value(forKey: UIResponder.keyboardFrameEndUserInfoKey) as! NSValue).cgRectValue.size
+       
+        let contentInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: kbSize.height, right: 0)
+       
+        scrollViewLogin.contentInset = contentInsets
+        scrollViewLogin.scrollIndicatorInsets = contentInsets
+    }
+    
+    @objc func keyboardWillHide(notification: Notification) {
+        scrollViewLogin.contentInset = UIEdgeInsets.zero
+        scrollViewLogin.scrollIndicatorInsets = UIEdgeInsets.zero
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+    }
+    
+    @objc func hideKeyBoard() {
+        scrollViewLogin.endEditing(true)
     }
 
+    @IBAction func login(_sender: UIButton) {
+        
+        guard let logintext = loginTextField.text else { return }
+        guard let passtext = passwordTextField.text else { return }
+   
+        if logintext == "Alex", passtext == "qwe77" {
+            print("successful authorization")
+        }
+        else {
+            print("uncorrect password or login, please retry")
+        }
+    }
 
 }
 
