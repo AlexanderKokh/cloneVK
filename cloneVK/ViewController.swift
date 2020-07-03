155fe3d1fbc0a3ad8f2edad2017f6bcb0e6b6bcd
IOS <alekokh@yandex.ru>
2020-07-03 20:27:39 +0400

    Initial Commit

---
+++ cloneVK/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cloneVK
+//
+//  Created by Alexander Kokh on 03.07.2020.
+//  Copyright © 2020 Alexander Kokh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
