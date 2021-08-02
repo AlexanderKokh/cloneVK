6aad240634ab1f23dac6280cbcef6d3b8960c8f5
IOS <alekokh@yandex.ru>
2021-08-02 22:34:27 +0300

    Добавлен swiftFormat и swiftlint

---
+++ CloneVK/CloneVK/SceneDelegate.swift
@@ -0,0 +1,8 @@
+// SceneDelegate.swift
+// Copyright © RoadMap. All rights reserved.
+
+import UIKit
+/// SceneDelegate
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+}
