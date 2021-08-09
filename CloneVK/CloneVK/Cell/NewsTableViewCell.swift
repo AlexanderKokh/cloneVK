a70505fb0c3bf185124f6c6af3bb365da904cbd7
IOS <alekokh@yandex.ru>
2021-08-10 02:45:12 +0300

    add news tableview and tableviewCell

---
+++ CloneVK/CloneVK/Cell/NewsTableViewCell.swift
@@ -0,0 +1,35 @@
+// NewsTableViewCell.swift
+// Copyright © RoadMap. All rights reserved.
+
+import UIKit
+
+final class NewsTableViewCell: UITableViewCell {
+    // MARK: - IBOutlet
+
+    @IBOutlet private var textNewsLabel: UILabel!
+    @IBOutlet private var avatarNewsImageView: UIImageView!
+    @IBOutlet private var avatarNewsLabel: UILabel!
+    @IBOutlet private var mainNewsImageView: UIImageView!
+
+    // MARK: - Initializers
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+    // MARK: - Public methods
+
+    func configureCell(news: News) {
+        avatarNewsImageView.contentMode = .scaleAspectFit
+        avatarNewsImageView.image = UIImage(named: news.sourceImageName)
+        avatarNewsLabel.text = news.sourceNews
+        textNewsLabel.text = news.sourceText
+        guard let mainNewsImage = news.sourceMainImagename else { return }
+        mainNewsImageView.contentMode = .scaleToFill
+        mainNewsImageView.image = UIImage(named: mainNewsImage)
+    }
+}
---
+++ CloneVK/CloneVK/Model/News.swift
@@ -0,0 +1,12 @@
+// News.swift
+// Copyright © RoadMap. All rights reserved.
+
+import Foundation
+
+/// Модель для заполнения новостей
+struct News {
+    let sourceNews: String
+    let sourceImageName: String
+    let sourceText: String
+    let sourceMainImagename: String?
+}
